6dcd03fddcf42dca73b3479c03a3029d219bb19d
lvdesign <mailbox@lvdesign.com.fr>
2019-06-14 19:27:50 +0200

    news

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
   production: true
 };
+"SANITY_TOKEN": "sk8TPQAVF9c9VJnE1Fau7hkA2ynvrF0J3JF6peuVTcrkTAFSPYYlcxOiG7sAL80qk8I2yknDhtcwE3onPoz5DD4VgoFNFzyK8x4eFQB4hhPFLt3Fsd4h9xynGqwLM9ukKBN9X2U3ZijSaswTk97Mq2Rtlb16lEabee1EygnNmOcfg7VzeLeJ"
+
+"SANITY_DATASET": "production"
+"SANITY_PROJECT_ID": "8iq7b4yz"
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,6 +4,11 @@
 
 export const environment = {
   production: false
+
+  "SANITY_TOKEN": "sk8TPQAVF9c9VJnE1Fau7hkA2ynvrF0J3JF6peuVTcrkTAFSPYYlcxOiG7sAL80qk8I2yknDhtcwE3onPoz5DD4VgoFNFzyK8x4eFQB4hhPFLt3Fsd4h9xynGqwLM9ukKBN9X2U3ZijSaswTk97Mq2Rtlb16lEabee1EygnNmOcfg7VzeLeJ"
+
+"SANITY_DATASET": "production"
+"SANITY_PROJECT_ID": "8iq7b4yz"
 };
 
 /*
